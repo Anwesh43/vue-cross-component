d4050d539255aac2f2b8195ee7024b5169d46db1
Anwesh43 <anweshthecool0@gmail.com>
2019-06-16 10:30:17 +0530

    adding State class

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+class State {
+
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale += 0.05 * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
+
 Vue.component('line', {
     props : ['rot'],
     template : '#linetemplate'
